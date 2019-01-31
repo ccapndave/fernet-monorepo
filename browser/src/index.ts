239bebbe31b1d8338d48c9b190e491c08d52b7cc
Dave Keen <dev@ruffness.com>
2019-01-31 11:19:31 +0100

    Prepared for individual npm releases

--- browser/src/index.ts
+++ browser/src/index.ts
@@ -1,11 +1,11 @@
 import { encrypt as sharedEncrypt, decrypt as sharedDecrypt } from "./passphrase";
 
-(<any>window).Buffer = require("buffer").Buffer;
+(<any>self).Buffer = require("buffer").Buffer;
 
 export async function encrypt(passphrase: string, plainText: string) {
-  return sharedEncrypt(window.crypto, passphrase, plainText);
+  return sharedEncrypt(self.crypto, passphrase, plainText);
 }
 
 export async function decrypt(passphrase: string, cipherText: string) {
-  return sharedDecrypt(window.crypto, passphrase, cipherText);
+  return sharedDecrypt(self.crypto, passphrase, cipherText);
 }
