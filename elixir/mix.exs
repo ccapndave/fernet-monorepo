8ed810bb57c9c0e348f33a9783c841ec39b4a4e5
Dave Keen <dev@ruffness.com>
2019-03-02 12:44:15 +0100

    Bumped Elixir version

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -4,7 +4,7 @@ defmodule Fernet.Passphrase.MixProject do
   def project do
     [
       app: :fernet_passphrase,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       description: description(),
